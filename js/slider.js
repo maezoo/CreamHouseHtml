aaa05bf7048a52ad90ce379f7723214377cbb426
maezoo <aa00456@naver.com>
2025-02-25 22:05:00 +0900

    Project: complete layout and styling

--- js/slider.js
+++ js/slider.js
@@ -1,24 +1,22 @@
-// var mainSwiper = new Swiper(".main-swiper", {
-//     pagination: {
-//         el: ".swiper-pagination",
-//         type: "progressbar",
-//     },
-//     mousewheel: true,
-//     navigation: {
-//         nextEl: ".swiper-button-next",
-//         prevEl: ".swiper-button-prev",
-//     },
-// });
-
 
 var mainSwiper = new Swiper(".main-swiper", {
     slidesPerView: 5,
     direction: getDirection(),
-
     mousewheel: true,
     on: {
         resize: function () {
-            swiper.changeDirection(getDirection());
+            mainSwiper.changeDirection(getDirection());
+        },
+    },
+});
+
+var projectSwiper = new Swiper(".project-slide", {  // 변수명 변경
+    slidesPerView: 5,
+    direction: getDirection(),
+    mousewheel: true,
+    on: {
+        resize: function () {
+            projectSwiper.changeDirection(getDirection());  // 변수명 변경
         },
     },
 });
@@ -26,45 +24,5 @@ var mainSwiper = new Swiper(".main-swiper", {
 function getDirection() {
     var windowWidth = window.innerWidth;
     var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
-
     return direction;
 }
-
-
-// var happySwiper = new Swiper(".Happy-swiper", {
-//     effect: "fade",
-//     pagination: {
-//         el: ".swiper-pagination",
-//         type: "fraction",
-//     },
-//     navigation: {
-//         nextEl: ".happy_btn_next",
-//         prevEl: ".happy_btn_prev",
-//         clickable: true,
-//     },
-//     loop: true,
-//     autoplay: true,
-
-// });
-// // ================================================
-
-// var brandSwiper = new Swiper(".brand-swiper", {
-//     effect: "fade",
-//     navigation: {
-//         nextEl: '.tab-next-1',
-//         prevEl: '.tab-prev-1',
-//         clickable: true,
-//     },
-//     loop: true,
-// });
-
-// // ================================================
-// var nowSwiper = new Swiper(".now-swiper", {
-//     effect: "fade",
-//     navigation: {
-//         nextEl: '.tab-next-2',
-//         prevEl: '.tab-prev-2',
-//         clickable: true,
-//     },
-//     loop: true,
-// });
